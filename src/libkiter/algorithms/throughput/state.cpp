213d95a06334978f2de614e2649e90a03f5c7cef
Jaime <jkmingwen@gmail.com>
2021-01-05 15:15:20 +0800

    Add State class

---
+++ src/libkiter/algorithms/throughput/state.cpp
@@ -0,0 +1,73 @@
+/*
+ * state.cpp
+ *
+ *  Created on: 3 January 2021
+ *      Author: jkmingwen
+ */
+
+#include "state.h"
+#include <models/Dataflow.h>
+
+State::State()
+  :executingActors(),
+   actorPhases(),
+   currentTokens(),
+   timeElapsed{0} {}
+
+State::State(models::Dataflow* const dataflow) {
+  {ForEachEdge(dataflow, e) {
+      currentTokens[e] = dataflow->getPreload(e);
+    }}
+  {ForEachTask(dataflow, t) {
+      actorPhases[t] = 1; // assume all actors start in first phase
+    }}
+  timeElapsed = 0;
+}
+
+// returns current phase of actor
+PHASE_INDEX State::getPhase(Vertex a) const {
+  return actorPhases.at(a);
+}
+
+// returns current tokens in edge
+TOKEN_UNIT State::getTokens(Edge e) const {
+  return currentTokens.at(e);
+}
+
+// returns amount of time left for execution
+TIME_UNIT State::getRemExecTime(Vertex a) const {
+  return executingActors.at(a);
+}
+
+// returns total elapsed time in state
+TIME_UNIT State::getTimeElapsed() const {
+  return timeElapsed;
+}
+
+// void State::updateState(models::Dataflow* const dataflow,
+//                         std::map<ARRAY_INDEX, Actor> actors) {
+  
+// }
+
+bool State::operator==(const State& s) const {
+  if (this->getTimeElapsed() != s.getTimeElapsed()) {
+    return false;
+  }
+  for (auto it = this->actorPhases.begin(); it != this->actorPhases.end(); it++) {
+    if (this->getPhase(it->first) != s.getPhase(it->first)) {
+      return false;
+    }
+  }
+  for (auto it = this->currentTokens.begin(); it != this->currentTokens.end(); it++) {
+    if (this->getTokens(it->first) != s.getTokens(it->first)) {
+      return false;
+    }
+  }
+  for (auto it = this->executingActors.begin(); it != this->executingActors.end(); it++) {
+    if (this->getRemExecTime(it->first) != s.getRemExecTime(it->first)) {
+      return false;
+    }
+  }
+  
+  return true;
+}
---
+++ src/libkiter/algorithms/throughput/state.h
@@ -0,0 +1,34 @@
+/*
+ * state.h
+ *
+ *  Created on: 3 January 2021
+ *      Author: jkmingwen
+ */
+
+#include <models/Dataflow.h>
+
+namespace models {
+  class Dataflow;
+}
+
+class State {
+ public:
+  State();
+  State(models::Dataflow* const dataflow);
+  PHASE_INDEX getPhase(Vertex a) const; // returns current phase of actor
+  TOKEN_UNIT getTokens(Edge e) const; // returns current tokens in edge
+  TIME_UNIT getRemExecTime(Vertex a) const; // returns amount of time left for execution
+  TIME_UNIT getTimeElapsed() const; // returns total elapsed time in state
+  /* void updateState(models::Dataflow* const dataflow, */
+  /*                  std::map<ARRAY_INDEX, Actor> actors); // updates state with current status of graph */
+  bool operator==(const State& s) const;
+  
+  /* bool addToVisitedStates(std::vector<State> &visitedStates); // checks list of visited states and adds to list if new state */
+  
+ private:
+  std::map<Vertex, TIME_UNIT> executingActors; // track actor's currently executing and the time before completing execution
+  std::map<Vertex, PHASE_INDEX> actorPhases; // track the current phases of each actor
+  std::map<Edge, TOKEN_UNIT> currentTokens; // track number of tokens in each channel
+  TIME_UNIT timeElapsed; // track time elapsed given state
+  
+};
