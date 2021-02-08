325158b038136b1c51554c6e6b964346c11e3aaa
Bruno <bruno.bodin@yale-nus.edu.sg>
2021-02-08 16:37:54 +0100

    Tagged function to speedup soon

--- src/libkiter/commons/commons.h
+++ src/libkiter/commons/commons.h
@@ -524,6 +524,44 @@ inline entier modulo(entier a, entier b) {
 
 
 
+template <typename entier>
+std::pair<entier,entier> extended_euclide (entier _a, entier _b, entier _c) {
+
+	// Kuṭṭaka, Aryabhata's algorithm for solving linear Diophantine equations in two unknowns
+
+
+	entier gcdab = std::gcd(_a,_b) ;
+
+	VERBOSE_ASSERT (_c % gcdab == 0, "No solution");
+
+	entier a = _a/ gcdab;
+	entier b = _b/ gcdab;
+	entier c = _c/ gcdab;
+
+	std::pair<entier,entier> r (a,b);
+	std::pair<entier,entier> s (1,0);
+	std::pair<entier,entier> t (0,1);
+
+    while (r.second != 0) {
+    	entier quotient = r.first / r.second;
+    	r = std::pair<entier,entier> ( r.second ,  r.first - quotient * r.second );
+    	s = std::pair<entier,entier> ( s.second ,  s.first - quotient * s.second );
+    	t = std::pair<entier,entier> ( t.second ,  t.first - quotient * t.second );
+    }
+
+    VERBOSE_INFO ( "A:" <<  _a << ", B:" <<  _b << ", C:" <<  _c );
+    VERBOSE_INFO ( "gcdab:" <<  gcdab );
+    VERBOSE_INFO ( "A:" <<  a << ", B:" <<  b << ", C:" <<  c );
+    VERBOSE_INFO ( "Bézout coefficients:" <<  s.first << "," <<  t.first );
+    VERBOSE_INFO ( "greatest common divisor:" <<  r.first);
+    VERBOSE_INFO ( "quotients by the gcd:" <<  t.second<< "," <<   s.second);
+
+    return std::pair<entier,entier>  ( (c * s.first), (c * t.first));
+
+}
+
+
+
 }// end of commons namespace
 
 #endif /* COMMONS_H_ */
