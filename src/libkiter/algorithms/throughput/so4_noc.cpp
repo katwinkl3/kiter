05c7230904379c490ab760916c4ee62695081874
katwinkl3 <kat-sun@hotmail.com>
2021-06-30 16:45:13 +0800

    passed sanity check

--- src/libkiter/algorithms/throughput/so4_noc.cpp
+++ src/libkiter/algorithms/throughput/so4_noc.cpp
@@ -10,42 +10,37 @@
 #include "actor.h"
 #include "state.h"
 #include "so4_noc.h"
-
+#include <printers/stdout.h>
 #include "../../models/Scheduling.h"
+#include "../scc.h"
 
-std::pair<TIME_UNIT, static_task_schedule_t> get_starts(std::vector<std::vector<TIME_UNIT>> periods){
-  std::vector<TIME_UNIT> res;
-  int period = 0;
-  for(std::size_t i = 0; i < periods.size(); ++i) {
-    res.push_back(periods[i][0]);
-    if (i == periods.size() - 1){
-      period = periods[i][1] - periods[0][0];
-    }
-  }
-  TIME_UNIT ped = period;
-  std::cout <<     " PERIOD=[ " << commons::toString(res) << "], " << "length = " << commons::toString(period) << std::endl;
-  return {ped, res};
-}
-
-void algorithms::so4_noc(models::Dataflow* const dataflow,   parameters_list_t param_list) {
-  
+std::pair<TIME_UNIT, scheduling_t> algorithms::computeComponentSo4Schedule(models::Dataflow* const dataflow,
+                                                 std::pair<ARRAY_INDEX, EXEC_COUNT> &minActorInfo, scheduling_t schedule) {
   VERBOSE_ASSERT(dataflow,TXT_NEVER_HAPPEND);
   VERBOSE_ASSERT(computeRepetitionVector(dataflow),"inconsistent graph");
-
   StateList visitedStates;
   EXEC_COUNT minRepFactor = INT_MAX; // EXEC_COUNT is of type long int, and LONG_MAX has same max value
   ARRAY_INDEX minRepActorId;
   TOKEN_UNIT minRepActorExecCount = 0;
   TIME_UNIT timeStep;
 
-  long slots; //TODO: take from input file instead
-  slots = 1; //hardcode
+  int periodic_state_idx;
+  TIME_UNIT thr;
+  bool end_check = false; // temp workaround before replacing with mathematical solution
+  int actors_left = 0; // seems more efficient to use a counter check than to check through the array
+  std::map<ARRAY_INDEX, TIME_UNIT> actors_check;
+  {ForEachTask(dataflow, t){
+    ++actors_left;
+    actors_check[dataflow->getVertexId(t)] = -1;
+  }}
+   long slots; //TODO: take from input file instead
+  slots = 2; //TODO: take from file 
   std::map<std::pair<ARRAY_INDEX, ARRAY_INDEX>, long> condition; //TODO: take from input file instead; rmb to convert from id
-  condition.insert({{1,2},0});//4}); //id = v+1, i think??
+  condition.insert({{1,2},1});//4}); //id = v+1, i think??
   // condition.insert({{1,3},0});
   condition.insert({{1,4},0});//1});
   // condition.insert({{2,1},0});
-  condition.insert({{2,3},0});//1});
+  condition.insert({{2,3},1});//1});
   // condition.insert({{2,4},1});//4});
   condition.insert({{3,1},0});//4});
   // condition.insert({{3,2},0});//1});
@@ -53,31 +48,29 @@ void algorithms::so4_noc(models::Dataflow* const dataflow,   parameters_list_t p
   // condition.insert({{4,1},0});//1});
   // condition.insert({{4,2},0});
   condition.insert({{4,3},0});//4});
-
-  TIME_UNIT curr_step = 0;  
-  std::map<ARRAY_INDEX, std::vector<TIME_UNIT>> statics; //initial phase
-  std::map<ARRAY_INDEX, std::vector<std::vector<TIME_UNIT>>> periodics; //periodic phase
+  scheduling_t task_schedule; // {vertex_id : {time_unit: [time_unit]}}
+  
+  TIME_UNIT curr_step = 0;
+  std::map<ARRAY_INDEX, std::vector<std::vector<TIME_UNIT>>> starts; //{task idx : [[state 0 starts], [state 1 ...]]}
+  std::map<ARRAY_INDEX, std::vector<TIME_UNIT>> state_start = {};  //{task idx : [ starts]}, 
   bool periodic_state = false;
   
   // initialise actors
   std::map<ARRAY_INDEX, Actor> actorMap;
   {ForEachTask(dataflow, t) {
       actorMap[dataflow->getVertexId(t)] = Actor(dataflow, t);
-      VERBOSE_INFO("\n");
     }}
-
   State prevState(dataflow, actorMap);
   State currState(dataflow, actorMap);
-
-  scheduling_t task_schedule; // {vertex_id : {time_unit: [time_unit]}}
- 
   {ForEachTask(dataflow, t) {
+      // track actor with lowest repetition factor (determines when states are stored)
       if (actorMap[dataflow->getVertexId(t)].getRepFactor() < minRepFactor) {
         minRepFactor = actorMap[dataflow->getVertexId(t)].getRepFactor();
         minRepActorId = actorMap[dataflow->getVertexId(t)].getId();
       }
     }}
  
+  minActorInfo = std::make_pair(minRepActorId, minRepFactor);
   // Start ASAP execution loop
   while (true) {
     {ForEachEdge(dataflow, e) {
@@ -89,20 +82,21 @@ void algorithms::so4_noc(models::Dataflow* const dataflow,   parameters_list_t p
           if (actorMap[dataflow->getVertexId(t)].getId() == minRepActorId) {
             minRepActorExecCount++;
             if (minRepActorExecCount == minRepFactor) {
-              // end initial
-              if (!periodic_state){periodic_state = true;}
-              if (!visitedStates.addState(currState)) {
-                VERBOSE_INFO("ending execution and computing throughput");
-                TIME_UNIT thr = visitedStates.computeThroughput();
-                std::cout << "throughput computed is: " << thr << std::endl;
-
-                {ForEachTask(dataflow, t2){
-                  task_schedule_t sched_struct = {statics[dataflow->getVertexId(t2)], get_starts(periodics[dataflow->getVertexId(t2)])};
-                  task_schedule.set(dataflow->getVertexId(t2), sched_struct);
+              
+              if (!end_check){
+                if (!visitedStates.addState(currState)) {
+                  VERBOSE_INFO("ending execution and computing throughput");
+                  // compute throughput using recurrent state
+                  
+                  thr = visitedStates.computeThroughput();
+                  periodic_state_idx = visitedStates.computeIdx(currState); //idx for repeated state
+                  end_check = true;
+                }
+              }
+              if (!end_check){
+                {ForEachTask(dataflow, task){ //start new state vector in starts
+                  starts[dataflow->getVertexId(task)].push_back({});
                 }}
-                models::Scheduling res = models::Scheduling(dataflow, 1/thr, task_schedule);
-                res.verbose_print();
-                return;
               }
               currState.setTimeElapsed(0);
               minRepActorExecCount = 0;
@@ -113,27 +107,131 @@ void algorithms::so4_noc(models::Dataflow* const dataflow,   parameters_list_t p
         }
       }}
    
-    
     // start actor firing
     {ForEachTask(dataflow, t) {
-        while (actorMap[dataflow->getVertexId(t)].isReadyForExecWithMod(dataflow, currState, condition, (int) curr_step % slots)) { //too heavy; modify datatype
-          actorMap[dataflow->getVertexId(t)].execStartWithMod(dataflow, currState, condition, (int) curr_step % slots); 
-        // while (actorMap[dataflow->getVertexId(t)].isReadyForExec(currState)) { //too heavy; modify datatype
-        //   actorMap[dataflow->getVertexId(t)].execStart(dataflow, currState); 
+        while (actorMap[dataflow->getVertexId(t)].isReadyForExecWithMod(dataflow, currState, condition, (int) curr_step % slots)) {
+          actorMap[dataflow->getVertexId(t)].execStartWithMod(dataflow, currState, condition, (int) curr_step % slots);
           currState.updateState(dataflow, actorMap);
-          if (periodic_state){
-            periodics[dataflow->getVertexId(t)].push_back({curr_step});
+
+          if (end_check){
+            if (actors_check[dataflow->getVertexId(t)] < 0){
+              actors_check[dataflow->getVertexId(t)] = curr_step;
+              starts[dataflow->getVertexId(t)].back().push_back(curr_step);
+              --actors_left;
+            }
+            if (actors_left == 0){
+              {ForEachTask(dataflow, task){ //Creates schedule after additional actor is fired after end of period
+                static_task_schedule_t initials; 
+                periodic_task_schedule_t periodics;
+                for(std::size_t i = 0; i < starts[dataflow->getVertexId(task)].size(); ++i){
+                  if (i < periodic_state_idx){ //initials
+                    initials.insert(initials.end(),starts[dataflow->getVertexId(task)][i].begin(),starts[dataflow->getVertexId(task)][i].end());
+                    continue;
+                  } 
+                  if (i == starts[dataflow->getVertexId(task)].size() - 1){
+                    periodics.second.insert(periodics.second.end(),starts[dataflow->getVertexId(task)][i].begin(),starts[dataflow->getVertexId(task)][i].end()-1);
+                  } else { //periodic
+                    periodics.second.insert(periodics.second.end(),starts[dataflow->getVertexId(task)][i].begin(),starts[dataflow->getVertexId(task)][i].end());
+                  }
+                }
+                periodics.first = actors_check[dataflow->getVertexId(task)] - periodics.second[0]; 
+                task_schedule_t sched_struct = {initials,periodics};
+                schedule.set(dataflow->getVertexId(task), sched_struct);
+                std::cout << dataflow->getVertexName(task) << ": initial starts=" << commons::toString(initials) << ", periodic starts=" << commons::toString(periodics) << std::endl;
+              }}
+              return std::make_pair(thr, schedule);
+            }
+
           } else {
-            statics[dataflow->getVertexId(t)].push_back(currState.getTimeElapsed());
+            if (starts[dataflow->getVertexId(t)].size() == 0){
+              starts[dataflow->getVertexId(t)].push_back({curr_step}); //push into current state's task start list
+            } else {
+              starts[dataflow->getVertexId(t)].back().push_back(curr_step);
+            }
           }
         }
       }}
-    
-    curr_step++;
-    timeStep = currState.advanceTimeWithMod();
-    if (timeStep == LONG_MAX) { //TODO: get a better fail safe this wont work now eheh
+    // advance time and check for deadlocks
+    timeStep = currState.advanceTime();
+    curr_step+= timeStep;
+    if (timeStep == LONG_MAX) { // NOTE should technically be LDBL_MAX cause TIME_UNIT is of type long double
       VERBOSE_INFO("Deadlock found!");
-      return; // should be returning 0
+      return std::make_pair(0, schedule);
     }
   }
+}
+
+void algorithms::scheduling::So4Scheduling(models::Dataflow* const dataflow,
+                                         parameters_list_t param_list) {
+  VERBOSE_ASSERT(dataflow,TXT_NEVER_HAPPEND);
+  VERBOSE_ASSERT(computeRepetitionVector(dataflow),"inconsistent graph");
+  std::map<int, std::vector<ARRAY_INDEX>> sccMap;
+  std::vector<models::Dataflow*> sccDataflows;
+  TIME_UNIT minThroughput = LONG_MAX; // NOTE should technically be LDBL_MAX cause TIME_UNIT is of type long double
+  
+  scheduling_t scheduling_result;
+  int linesize = param_list.count("LINE")? commons::fromString<int>(param_list["LINE"]) : 80;
+
+  // generate SCCs if any
+  sccMap = computeSCCKosaraju(dataflow);
+  if (sccMap.size() > 1) { // if the original graph isn't one SCC, need to break into SCC subgraphs
+    sccDataflows = generateSCCs(dataflow, sccMap);
+    for (auto g : sccDataflows) {
+      if (g->getEdgesCount() > 0) {
+        std::pair<ARRAY_INDEX, EXEC_COUNT> actorInfo;
+        auto res_pair = computeComponentSo4Schedule(g, actorInfo, scheduling_result);
+        TIME_UNIT componentThroughput = res_pair.first;
+        scheduling_result = res_pair.second;
+        TIME_UNIT scaledThroughput = (componentThroughput * actorInfo.second) /
+          dataflow->getNi(dataflow->getVertexById(actorInfo.first));
+        if (scaledThroughput < minThroughput) {
+          minThroughput = scaledThroughput;
+        }
+      } else if (g->getVerticesCount() == 1 && g->getEdgesCount() == 0) {
+        /* NOTE this is a workaround from ignoring reentrancy edges --- if this
+           condition is met, we assume that we have a single actor with re-entrancy */
+        ARRAY_INDEX standaloneId = g->getVertexId(g->getFirstVertex());
+        EXEC_COUNT standaloneRepFactor = g->getPhasesQuantity(g->getFirstVertex());
+        Vertex vertexInDataflow = dataflow->getVertexById(standaloneId);
+        /* repetition factor for standalone component will be equal to its phase count:
+           this makes sense because, while it's producing and consuming 1 token in its
+           re-entrant edge, it will need to execute its number of phases to arrive
+           back at the same state */
+        EXEC_COUNT repFactor = dataflow->getNi(vertexInDataflow);
+        TIME_UNIT componentThroughput = (TIME_UNIT) (1 * standaloneRepFactor) /
+          dataflow->getVertexTotalDuration(vertexInDataflow);
+        TIME_UNIT scaledThroughput = (TIME_UNIT) componentThroughput / repFactor;
+        if (scaledThroughput < minThroughput) {
+          minThroughput = scaledThroughput;
+        }
+      }
+    }
+    
+    TIME_UNIT omega = 1.0 / minThroughput ;
+    models::Scheduling res = models::Scheduling(dataflow, omega, scheduling_result);
+
+    std::cout << res.asASCII(linesize);
+    std::cout << res.asText();
+    std::cout << printers::Scheduling2Tikz(res);
+
+    std::cout << "ASAP throughput is  " << minThroughput << std::endl;
+    std::cout << "ASAP period is  " << omega << std::endl;
+
+    return;
+  }
+  // if graph is strongly connected, just need to use computeComponentThroughput
+  std::pair<ARRAY_INDEX, EXEC_COUNT> actorInfo; // look at note for computeComponentThroughput
+  auto res_pair = computeComponentSo4Schedule(dataflow, actorInfo, scheduling_result);
+  minThroughput = res_pair.first;
+  scheduling_result = res_pair.second;
+  std::cout << "Throughput of graph: " << minThroughput << std::endl;
+  TIME_UNIT omega = 1.0 / minThroughput ;
+  models::Scheduling res = models::Scheduling(dataflow, omega, scheduling_result);
+  std::cout << res.asASCII(linesize);
+  std::cout << res.asText();
+
+  std::cout << "ASAP throughput is  " << minThroughput << std::endl;
+  std::cout << "ASAP period is  " << omega << std::endl;
+
+  return;
 }
