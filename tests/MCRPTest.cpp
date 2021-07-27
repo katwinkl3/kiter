5735c00bbc6d024babcb80edc571319896d6eb4f
Bruno <bruno.bodin@yale-nus.edu.sg>
2021-07-27 14:37:33 +0800

    waffle the test

--- tests/MCRPTest.cpp
+++ tests/MCRPTest.cpp
@@ -20,9 +20,11 @@
 
 /* how to define bound? */
 
-BOOST_FIXTURE_TEST_SUITE( MCRP_test_suite , WITH_VERBOSE)
+BOOST_FIXTURE_TEST_SUITE( MCRP_test_suite , WITHOUT_VERBOSE)
 
 long event_graph_performance_tester (models::Dataflow* dataflow){
+
+
     VERBOSE_ASSERT(dataflow,TXT_NEVER_HAPPEND);
 	VERBOSE_ASSERT(computeRepetitionVector(dataflow),"inconsistent graph");
 	auto kvector = algorithms::get_Kvector(dataflow);
@@ -57,11 +59,16 @@ long MCRP_performance_tester (models::Dataflow* dataflow){
 
 BOOST_AUTO_TEST_CASE( generate_event_graph__performance_test )
 {
-	// for (int i = MAX_ITER/10; i <= MAX_ITER; i += MAX_ITER/10) {
-	// 	int buf_num = std::rand() % (i/2) + i; 
-	// 	models::Dataflow*  g = generate_random_graph(i, buf_num,  MAX_PHASE_COUNT, i, i);
-	// 	std::cout << event_graph_performance_tester(g);
-	// };
+
+	commons::set_verbose_mode(commons::WARNING_LEVEL);
+	for (int i = MAX_ITER/10; i <= MAX_ITER; i += MAX_ITER/10) {
+		int buf_num = std::rand() % (i/2) + i;
+		std::cout << "Iteration " << i << " buf_num=" << buf_num << std::endl;
+		models::Dataflow*  g = generate_random_graph(i, buf_num,  MAX_PHASE_COUNT, i, i);
+		std::cout << "Graph done " << i << std::endl;
+		std::cout << event_graph_performance_tester(g) << std::endl;
+		std::cout << "event_graph_performance_tester done " << i << std::endl;
+	};
 }
 
 BOOST_AUTO_TEST_CASE( MCRP_performance_test )
